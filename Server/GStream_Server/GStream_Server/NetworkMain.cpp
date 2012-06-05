b75c61cccd06475d9573272fdb258215295b73b2
ArkyChan <arky1417@gmail.com>
2012-06-05 20:20:43 +0100

    Forgot the net work files *blush*

---
+++ Server/GStream_Server/GStream_Server/NetworkMain.cpp
@@ -0,0 +1,16 @@
+//========================================================
+// A The network Nexus, hadles reciveing of all client 
+// commands and send data to the client
+//========================================================
+
+#include "NetworkMain.h"
+
+void net_startServer(int port)
+{
+
+}
+
+void net_stopServer()
+{
+
+}
---
+++ Server/GStream_Server/GStream_Server/NetworkMain.h
@@ -0,0 +1,10 @@
+#ifndef NETWORKMAIN_H
+#define NETWORKMAIN_H
+
+// Start the network server
+void net_startServer(int port);
+
+// Stop the server
+void net_stopServer();
+
+#endif
